6e8a005bb1431f23e5b6f1f2c0f7c7c1939e5566
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2016-07-28 21:48:01 +0900

    implement DFS

--- src/graph/AdjacencyMatrixGraph.java
+++ src/graph/AdjacencyMatrixGraph.java
@@ -1,5 +1,8 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class AdjacencyMatrixGraph {
 	private int size;
 	private String[] vertices;
@@ -23,6 +26,31 @@ public class AdjacencyMatrixGraph {
 		for(int i = 1; i < size; i++)
 			buf.append("," + vertex(i));
 		return buf + "}";
+	}
+	
+	public void DFS(String v){
+		Stack<String> stack = new Stack<String>();
+		ArrayList<String> list = new ArrayList<String>();
+		boolean[] visit = new boolean[size];
+		
+		visit[index(v)] = true;
+		stack.push(vertices[index(v)]);
+		
+		while(!stack.isEmpty()){
+			String x = stack.pop();
+			list.add(x);
+			for(int i = 0; i < size; i++){
+				if(adjacencyMatrix[index(x)][i] && !visit[i]){
+					visit[i] = true;
+					stack.push(vertices[i]);
+				}
+			}
+		}
+		
+		StringBuffer buf = new StringBuffer("{" + list.get(0));
+		for(int i = 1; i < list.size(); i++)
+			buf.append(" -> " + list.get(i));
+		System.out.println( buf + "}");
 	}
 
 	private String vertex(int i) {
